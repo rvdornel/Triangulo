84d4b4e2e3613fbff425ba2a9dcebe28df972bb9
rvdornel <rvdornel@ucs.br>
2023-01-09 14:39:55 -0300

    primeira

---
+++ src/triangulo/Application.java
@@ -0,0 +1,9 @@
+package triangulo;
+
+public class Application {
+	public static void main()
+	{
+		
+	}
+
+}
---
+++ src/triangulo/Triangulo.java
@@ -0,0 +1,17 @@
+package triangulo;
+
+public class Triangulo {
+
+	public double width;
+	public double height;
+	
+	public double area()
+	{
+		return width*height/2;
+	}
+	
+	public double perimeter() {
+		
+	}
+	
+}
